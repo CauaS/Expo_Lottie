78ea5a2af95883808248b5ceb98f9b78b114e9cc
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-01-03 12:12:23 -0200

    Tests
    Created tests to retrieve and  to register

--- src/cli/test.js
+++ src/cli/test.js
@@ -9,14 +9,20 @@ describe('Heroes Manipulation Switch', () => {
 
     it('Should retrieve a heroe by files', async () => {
         const expected = DEFAULT_ITEM_INPUT;
-        const [result] = await database.listar(expected.id);
+        const [result] = await database.retrieveData(expected.id);
         
         deepEqual(result, expected);
     })
-    /*
+    
     it('Should register heroes by files', async () => {
         const expected = DEFAULT_ITEM_INPUT;
-        of(null, expected);
+
+        // result will receive true or false
+        const result = await database.registerHeroe(DEFAULT_ITEM_INPUT); 
+
+        const [actual] = await database.retrieveData(DEFAULT_ITEM_INPUT.id);
+
+        deepEqual(actual, expected);
     })
-    */
+    
 })
